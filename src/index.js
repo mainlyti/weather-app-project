27ce34820086fed9caa2356036904d96023387df
mainlyti <mainlyti@gmail.com>
2025-04-30 22:48:01 -0400

    added search function with api

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
+//Displays current temperature
+function currentTemperature(response) {
+  let temperatureElement = document.querySelector("#current-temperature-value");
+  let temperature = Math.round(response.data.temperature.current);
+  temperatureElement.innerHTML = temperature;
+
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = response.data.city;
+}
+
+//API
+function searchCity(city) {
+  let apiKey = "5bfa080034fb64td6b864b3a813efo04";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(currentTemperature);
+}
+
 //Display searched city
 function search(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
-  let cityElement = document.querySelector("#current-city");
-  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", search);
+
+searchCity("New York");
