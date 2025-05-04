5cc8e84c63c257edbeb678d069a708f9a33fe42b
mainlyti <mainlyti@gmail.com>
2025-05-04 14:10:09 -0400

    added current time

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-//Displays current temperature and condition
+//Displays current temperature, condition, humidity, wind speed and date
 function currentTemperature(response) {
   let temperatureElement = document.querySelector("#current-temperature-value");
   let temperature = Math.round(response.data.temperature.current);
@@ -6,12 +6,41 @@ function currentTemperature(response) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#wind-speed");
+  let dateElement = document.querySelector("#current-date");
+  let date = new Date(response.data.time * 1000);
 
   cityElement.innerHTML = response.data.city;
+  dateElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}mph`;
   temperatureElement.innerHTML = temperature;
+}
+
+//Formats date
+function formatDate(date) {
+  let hour = date.getHours();
+  let minute = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minute < 10) {
+    minute = `0${minute}`;
+  }
+
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+
+  return `${day} ${hour}:${minute}`;
 }
 
 //API
