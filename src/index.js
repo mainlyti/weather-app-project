f17d3dfecd151489cbf3933d93dfe530376d9692
mainlyti <mainlyti@gmail.com>
2025-05-04 13:40:53 -0400

    Added humidity, conditions, and wind speed to js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-//Displays current temperature
+//Displays current temperature and condition
 function currentTemperature(response) {
   let temperatureElement = document.querySelector("#current-temperature-value");
   let temperature = Math.round(response.data.temperature.current);
-  temperatureElement.innerHTML = temperature;
-
   let cityElement = document.querySelector("#current-city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+
   cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}mph`;
+  temperatureElement.innerHTML = temperature;
 }
 
 //API
